a2e9f199e75018e1be652db186d3c113b40acbc7
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2025-06-06 13:05:04 -0300

    implementando lbp simples

--- include/lbp/lbp.hpp
+++ include/lbp/lbp.hpp
@@ -26,10 +26,24 @@ namespace lbp_library
         {
             // TODO: Montar o kernel e fazer o histograma com o valor do kernel, depois vou querer plotar ele
             // Estou pegando o canal 1, de escala de cinza e não RGB
-            for (int i = 0; i < src.rows; ++i) {
-                for (int j = 0; j < src.cols; ++j) {
+            for (int i = 1; i < src.rows; ++i) {
+                for (int j = 1; j < src.cols; ++j) {
                     // Faço essa transformação se não vem caracter ascii
+                    uchar center = src.at<uchar>(i, j);
                     std::cout << static_cast<int>(src.at<uchar>(i, j)) << " ";
+                    
+                    uchar code = 0;
+                    // kernel 3x3 | Fazer isso de forma mais inteligente, pois quero passar o kernel como parametro
+                    code |= (src.at<uchar>(i-1, j-1) >= center) << 7; // Canto superior esquerdo
+                    code |= (src.at<uchar>(i-1, j  ) >= center) << 6; // canto esquerdo
+                    code |= (src.at<uchar>(i-1, j+1) >= center) << 5; // canto inferior esquerdo
+                    code |= (src.at<uchar>(i,   j+1) >= center) << 4; // canto superior
+                    code |= (src.at<uchar>(i+1, j+1) >= center) << 3; // canto superior direito
+                    code |= (src.at<uchar>(i+1, j  ) >= center) << 2; // canto direito
+                    code |= (src.at<uchar>(i+1, j-1) >= center) << 1; // canto inferior direito
+                    code |= (src.at<uchar>(i,   j-1) >= center) << 0; // canto inferior
+
+                    // Adicionar o code em uma estrutura do número binário montado
                 }
                 std::cout << std::endl;
             }
